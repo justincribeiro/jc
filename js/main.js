8e96867a6b67f047b8956466265ec89120a1fd3c
Justin <justincribeiro@gmail.com>
2020-09-19 01:11:39 -0400

    first commit!

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*jslint browser: true*/
+/*global $, jQuery*/
+
+$(document).ready(function () {
+	$("body").removeClass("preload");
+	
+	$.getJSON('https://api.sheety.co/467efedf-6454-491d-a6c5-a31383447ec3', function(data) {
+		var template = Handlebars.compile($('#item-template').html())
+		$('#items').html(template(data))
+	})
+});
